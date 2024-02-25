7313b050d9c5c702042d628c544a11b5431bed97
vaguiiinho <avanlendario@gmail.com>
2024-02-25 11:10:57 -0300

    Separando frameworks and drivers de interface adapters

---
+++ infrastructure/src/main/java/br/com/fullcycle/infrastructure/rest/PartnerFnController.java
@@ -0,0 +1,53 @@
+package br.com.fullcycle.infrastructure.rest;
+
+import java.net.URI;
+import java.util.Objects;
+
+import br.com.fullcycle.application.partner.CreatePartnerUseCase;
+import br.com.fullcycle.application.partner.GetPartnerByIdUseCase;
+import br.com.fullcycle.domain.exceptions.ValidationException;
+import br.com.fullcycle.infrastructure.dtos.NewPartnerDTO;
+import br.com.fullcycle.infrastructure.http.HttpRouter;
+import br.com.fullcycle.infrastructure.http.HttpRouter.HttpRequest;
+import br.com.fullcycle.infrastructure.http.HttpRouter.HttpResponse;
+
+// Adapter
+public class PartnerFnController {
+
+    private final CreatePartnerUseCase createPartnerUseCase;
+    private final GetPartnerByIdUseCase getPartnerByIdUseCase;
+
+    public PartnerFnController(
+            final CreatePartnerUseCase createPartnerUseCase,
+            final GetPartnerByIdUseCase getPartnerByIdUseCase
+    ) {
+        this.createPartnerUseCase = Objects.requireNonNull(createPartnerUseCase);
+        this.getPartnerByIdUseCase = Objects.requireNonNull(getPartnerByIdUseCase);
+    }
+
+    public HttpRouter bind(final HttpRouter router) {
+        router.GET("/partners/{id}", this::get);
+        router.POST("/partners", this::create);
+        return router;
+    }
+
+    private HttpResponse<?> create(final HttpRequest req) {
+        try {
+            final var dto = req.body(NewPartnerDTO.class);
+
+            final var output =
+                    createPartnerUseCase.execute(new CreatePartnerUseCase.Input(dto.cnpj(), dto.email(), dto.name()));
+
+            return HttpResponse.created(URI.create("/partners/" + output.id())).body(output);
+        } catch (ValidationException ex) {
+            return HttpResponse.unprocessableEntity().body(ex.getMessage());
+        }
+    }
+
+    private HttpResponse<?> get(final HttpRequest req) {
+        final String id = req.pathParam("id");
+        return getPartnerByIdUseCase.execute(new GetPartnerByIdUseCase.Input(id))
+                .map(HttpResponse::ok)
+                .orElseGet(HttpResponse.notFound()::build);
+    }
+}
